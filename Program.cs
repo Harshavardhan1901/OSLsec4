60f864d440cb91dc26b0f996d25010def059f5bb
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-07-23 21:21:18 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Sec4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\Student_data.txt";
+            if (File.Exists(filePath))
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                Console.WriteLine("Student Data:");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: The student_data.txt file is missing.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
